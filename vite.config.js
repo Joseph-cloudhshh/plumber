11910f468c4f5504b0de846386e05c73a30c7018
Elizabeth Stark <akashimunto@gmail.com>
2026-05-03 22:28:27 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,9 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import base44 from '@base44/vite-plugin'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  logLevel: 'error',
-  plugins: [
-    base44({
-      legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === 'true',
-      hmrNotifier: false,
-      navigationNotifier: false,
-      analyticsTracker: false,
-      visualEditAgent: false
-    }),
-    react(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
